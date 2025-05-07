4813b33db28a44bb83dec7b7a4a2773e52bd6e5a
Sato1622 <kevin.alexisc902@gmail.com>
2025-05-07 01:42:11 -0300

    Add files via upload

---
+++ Test3.lua
@@ -0,0 +1,6 @@
+local PlayerP = game.Players.LocalPlayer
+local PlayerW = game.Workspace:WaitForChild(PlayerP.Name)
+
+local Part = Instance.new("Part")
+Part.Name = "Block"
+Part.Parent = game.Workspace
