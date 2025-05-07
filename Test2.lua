d5fa66363c41c322cb029fc5a82cc6be5baa48ff
Sato1622 <kevin.alexisc902@gmail.com>
2025-05-07 01:32:01 -0300

    Add files via upload

---
+++ Test2.lua
@@ -0,0 +1,9 @@
+local PlayerP = game.Players.LocalPlayer
+local PlayerW = game.Workspace:WaitForChild(PlayerP.Name)
+local LeftLeg = PlayerW:WaitForChild("Left Leg")
+
+
+local Part = Instance.new("Part")
+Part.Name = "Block"
+Part.Parent = game.Workspace
+Part.Position = LeftLeg.Position
