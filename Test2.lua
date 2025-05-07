1533ac65669e8c783c6b3a62f50050847d57bcca
Sato1622 <kevin.alexisc902@gmail.com>
2025-05-07 09:03:43 -0300

    Delete Test2.lua

--- Test2.lua
+++
@@ -1,7 +0,0 @@
-local PlayerP = game.Players.LocalPlayer
-local PlayerW = game.Workspace:WaitForChild(PlayerP.Name)
-
-local Part = Instance.new("Part")
-Part.Name = "Block"
-Part.Parent = game.Workspace
-
