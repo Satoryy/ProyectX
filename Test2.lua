469e8310dde15aeef8c337cdb4ae8c11b34e3091
Sato1622 <kevin.alexisc902@gmail.com>
2025-05-07 01:40:17 -0300

    Test2.lua

--- Test2.lua
+++ Test2.lua
@@ -1,7 +1,5 @@
 local PlayerP = game.Players.LocalPlayer
 local PlayerW = game.Workspace:WaitForChild(PlayerP.Name)
-local LeftLeg = PlayerW:WaitForChild("Left Leg")
-
 
 local Part = Instance.new("Part")
 Part.Name = "Block"
